f9d04d14561039fd51721dfb852f19a21cae0d46
Matt Briselli <MattBriselli@gmail.com>
2017-03-25 22:27:45 -0400

    facebook login working

--- koffEE/ViewController.swift
+++ koffEE/ViewController.swift
@@ -11,25 +11,40 @@ import Firebase
 import FirebaseAuth
 import FacebookCore
 import FacebookLogin
+import FacebookShare
 
 class ViewController: UIViewController {
-
+    
     override func viewDidLoad() {
-        super.viewDidLoad()
         
-        let loginButton = LoginButton(readPermissions: [ .publicProfile, .userFriends, .email ])
+        let auth = FIRAuth.auth()
+        
+        let loginButton = UIButton(type: .custom)
+        loginButton.backgroundColor = UIColor.darkGray
         loginButton.center = view.center
+        loginButton.frame = CGRect(x: view.center.x-90, y: view.center.y, width: 180, height: 45)
+        loginButton.setTitle("FB Login", for: .normal)
+        loginButton.addTarget(self, action: #selector(loginButtonClicked(button:)), for: .touchUpInside)
         
         view.addSubview(loginButton)
-        
-        if let accessToken = AccessToken.current {
-            // User is logged in, use 'accessToken' here.
-            print("good")
-        } else {
-            print("bad")
-        }
-        
     }
+    
+    @objc func loginButtonClicked(button: UIButton) {
+        let loginManager = LoginManager()
+        loginManager.loginBehavior = LoginBehavior.systemAccount
+        loginManager.logIn([ .publicProfile, .email, .userFriends ], viewController: self) { loginResult in
+            switch loginResult {
+            case .failed(let error):
+                print(error)
+            case .cancelled:
+                print("User cancelled login.")
+            case .success(let grantedPermissions, let declinedPermissions, let accessToken):
+                let credential = FIRFacebookAuthProvider.credential(withAccessToken: accessToken.authenticationToken)
+                print(credential)
+            }
+        }
+    }
+
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
